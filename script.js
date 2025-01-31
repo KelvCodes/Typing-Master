1bf295ff0fa827874a439ba287e00952c2e4a616
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-01-31 15:37:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // DOM Elements
 const word = document.getElementById("word");// Element to display the random word
 const text = document.getElementById("text");// Input field for typing the word
-const scoreElement = document.getElementById("score");
+const scoreElement = document.getElementById("score");// Element to display the score
 const timeElement = document.getElementById("time");
 const endgameElement = document.getElementById("end-game-container");
 const settingsButton = document.getElementById("settings-btn");
