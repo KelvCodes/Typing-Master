e27e6d6493bdd9090bfd6ef2a15f5e4881f29c5c
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-03-19 15:50:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-splay = "flex"; // Show the end game container
-}
-
-// Event listener for the input field
-text.addEventListener("input", (e) => {
-  const insertedText = e.target.value;// Get the text entered by the user
-  if (insertedText === randomWord) {
-    e.target.value = ""; // Clear the input field
-    addWordToDom();// Display a new random word
-    updateScore();
-    if (difficulty === "hard") time += 2;
-    else if (difficulty === "medium") time += 3;
-    else time += 5;
-    updateTime();
-  }
-});
-
-settingsButton.addEventListener("click", () =>
-  settings.classList.toggle("hide")
-);
-settingsForm.addEventListener("change", (e) => {
-  difficulty = e.target.value;
+ = e.target.value;
   localStorage.setItem("difficulty", difficulty);
 });
 
